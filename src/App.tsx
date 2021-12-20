74f6bc524a850e71053f98875932befbf3f12b80
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2021-12-20 22:29:36 +0900

    feat: add filter, icons and bottom bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,59 @@ import Container from "react-bootstrap/Container";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Profile from './pages/Profile/Profile';
+import Chat from './pages/Chat/Chat';
 import SearchList from './pages/SearchList';
+import { FiSearch, FiHome, FiMessageSquare, FiUser, FiBell, FiUserPlus } from 'react-icons/fi';
+import styles from './App.module.css';
 
 function App() {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Nav className="me-auto">
-            <Link className="nav-link" to="/">Home</Link>
-            <Link className="nav-link" to="/profile">Profile</Link>
-            <Link className="nav-link" to="/search-list">Search Result</Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
+      <header style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        backgroundColor: 'lightpink',
+      }}>
+        <FiBell size={30} />
+        <h2>Parents</h2>
+        <FiUser size={30} />
+      </header>
       <hr style={{ margin: 0 }} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="profile" element={<Profile />} />
         <Route path="search-list" element={<SearchList />} />
+        <Route path="chat" element={<Chat />} />
       </Routes>
-    </div>
+
+
+      <footer style={{
+        position: 'fixed',
+        width: '100%',
+        bottom: 0,
+      }}>
+        <hr style={{ margin: 0 }} />
+        <Navbar bg="light" expand="lg" style={{ padding: 0 }}>
+          <Container>
+            <Nav className="me-auto" style={{ 'flexDirection': 'row', flex: 1, justifyContent: 'center' }}>
+              <Link className={`nav-link ${styles.link}`} to="/">
+                <FiHome size={30} />
+                Home
+              </Link>
+              <Link className={`nav-link ${styles.link}`} to="/search-list">
+                <FiSearch size={30} />
+                Search
+              </Link>
+              <Link className={`nav-link ${styles.link}`} to="/profile"><FiUserPlus size={30} />Add</Link>
+              <Link className={`nav-link ${styles.link}`} to="/chat">
+                <FiMessageSquare size={30} />
+                Chat
+              </Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </footer>
+    </div >
   );
 }
 
--- src/pages/SearchList/SearchList.tsx
+++ src/pages/SearchList/SearchList.tsx
@@ -1,8 +1,99 @@
+import { useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { parents as mockParents } from '../../mockData/parents';
+import { FiToggleLeft, FiToggleRight } from 'react-icons/fi';
+import styles from './styles.module.css';
+
+type Filter = {
+  id: number;
+  name: string;
+  state: boolean;
+  order: number;
+};
+
+type ModalWrapperProps = {
+  show: boolean;
+  handleClose: () => void;
+  filterClick: (id: number) => void;
+  filters: Filter[];
+};
+
+const baseFilters: Record<number, Filter> = {
+  1: {
+    id: 1,
+    name: 'Filter 1',
+    state: false,
+    order: 1,
+  },
+  2: {
+    id: 2,
+    name: 'Filter 2',
+    state: false,
+    order: 2,
+  },
+  3: {
+    id: 3,
+    name: 'Filter 3',
+    state: false,
+    order: 3,
+  },
+};
+
+const ModalWrapper = ({ show, handleClose, filters, filterClick }: ModalWrapperProps) => {
+  return (
+    <Modal show={show} onHide={handleClose} fullscreen>
+      <Modal.Header closeButton>
+        <Modal.Title>Filters</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <ListGroup>
+          {filters.map(x => (
+            <ListGroup.Item key={x.id} className={styles.listItem} onClick={() => {
+              filterClick(x.id);
+            }}>
+              {x.name}
+              {x.state ? <FiToggleRight /> : <FiToggleLeft />}
+
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary">Close</Button>
+        <Button variant="primary">Save changes</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
 const SearchList = () => {
+  const [show, setShow] = useState(false);
+  const onHandleClose = () => {
+    setShow(false);
+  };
+
+  const onFiltersClick = () => {
+    setShow(true);
+  };
+
+  const onFilterClick = (id: number) => {
+    const newFilters: Record<number, Filter> = {
+      ...filters,
+      [id]: {
+        ...filters[id],
+        state: !filters[id].state,
+      }
+    };
+    setFilters(newFilters);
+  };
+
+  const [filters, setFilters] = useState(baseFilters);
+
   return (
     <div>
       <h1>Results</h1>
@@ -14,6 +105,8 @@ const SearchList = () => {
           </ListGroup.Item>
         ))}
       </ListGroup>
+      <Button variant="primary" onClick={onFiltersClick}>Filters</Button>
+      <ModalWrapper show={show} handleClose={onHandleClose} filters={Object.values(filters)} filterClick={onFilterClick} />
     </div>
   );
 };
